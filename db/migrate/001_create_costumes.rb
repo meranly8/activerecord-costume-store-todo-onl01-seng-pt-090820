5830d2ab0626dca83fbed8a63f8d86325083de77
Michelle Ranly <meranly8@gmail.com>
2020-11-18 06:11:06 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,6 +11,10 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
+      t.integer :price
+      t.text :image_url
+      t.string :size
+      t.timestamp
     end
   end
 end
