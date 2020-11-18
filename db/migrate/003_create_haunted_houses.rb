19cbce61540dd66adffbedf274c4d0deef33f2f7
Michelle Ranly <meranly8@gmail.com>
2020-11-18 04:13:11 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,6 +1,8 @@
 # Create your haunted_houses migration here
 class HauntedHouses < ActiveRecord::Migration[5.2]
   def change
-
+    create_table :haunted_houses do |t|
+      t.string :name 
+    end
   end
 end
