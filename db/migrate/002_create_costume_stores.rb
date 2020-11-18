c387bc5eb64efe48fa0009cf0ced5886567f3379
Michelle Ranly <meranly8@gmail.com>
2020-11-18 06:17:52 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,6 +3,11 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change
     create_table :costume_stores do |t|
       t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.boolean :in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
