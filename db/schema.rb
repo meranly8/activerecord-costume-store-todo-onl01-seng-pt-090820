85a23f42c53987656346555a609793d3ff1ed06d
Michelle Ranly <meranly8@gmail.com>
2020-11-18 06:18:09 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 3) do
+
+  create_table "costume_stores", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "costume_inventory"
+    t.boolean "in_business"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
+  end
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
@@ -19,6 +28,17 @@ ActiveRecord::Schema.define(version: 1) do
     t.string "size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "haunted_houses", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "theme"
+    t.integer "price"
+    t.boolean "family_friendly"
+    t.datetime "opening_date"
+    t.datetime "closing_date"
+    t.text "description"
   end
 
 end
